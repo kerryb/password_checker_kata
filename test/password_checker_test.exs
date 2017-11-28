3f46098e3c7ea45a9d2bd74868087e7484769bfe
Kerry Buckley <kerryjbuckley@gmail.com>
2017-11-28 14:17:21 +0000

    Extract test helper

--- test/password_checker_test.exs
+++ test/password_checker_test.exs
@@ -7,6 +7,10 @@ defmodule PasswordCheckerTest do
   end
 
   test "password must be longer than eight characters" do
-    assert PasswordChecker.check("abcdefgh") |> Enum.find(& &1 == "must be longer than eight characters")
+    check_has_error("abcdefgh", "must be longer than eight characters")
+  end
+
+  defp check_has_error(password, message) do
+    assert PasswordChecker.check(password) |> Enum.find(& &1 == message)
   end
 end
